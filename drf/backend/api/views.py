a0aab95b55d46c49c65e313570818bf5624d2597
PyreX00 <Subedianish2022@gmail.com>
2025-08-25 14:38:56 +0545

    added model

--- drf/backend/api/views.py
+++ drf/backend/api/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+import json
 
 # Create your views here.
 
 def hello(request, *args,**kwargs):
-    return JsonResponse({"message":"Hello"})
+    body = request.body
+    data = {}
+    
+    try:
+        data = json.loads(body)
+    except:
+        return JsonResponse({"Message":"Data recieved failed"})
+    
+    print(data)
+    data['headers'] = request.headers
+    
+    data['content_type'] = request.content_type
+    print(data)
+    return JsonResponse({"Message":"Data recieved"})
     
--- drf/py_client/basic.py
+++ drf/py_client/basic.py
@@ -3,8 +3,9 @@ import requests
 # endpoint ="https://httpbin.org/status/200/"
 # endpoint ="https://httpbin.org/anything"
 
-endpoint = "http://127.0.0.1:8000/hello/"
+endpoint = "http://127.0.0.1:8000/api/hello/"
 
 
-get_response = requests.get(endpoint)
-print(get_response.text)
+get_response = requests.get(endpoint,params={"abc":123},json ={"query":"Hello world"})
+print(get_response.json()["Message"])
+print(get_response.status_code)
